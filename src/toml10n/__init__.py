91ba5110ba65978c09279d83418ac1ef88fbaba7
Pon4ick <ikonstantin2010@gmail.com>
2026-04-24 17:50:19 +0300

    python version fix

--- src/toml10n/__init__.py
+++ src/toml10n/__init__.py
@@ -1,4 +1,4 @@
 from .manager import LocalizationManager, LocaleDirNotFoundError, LanguageNotFoundError, LocaleFileLoadError
 
 __all__ = ["LocalizationManager", "LocaleDirNotFoundError", "LanguageNotFoundError", "LocaleFileLoadError"]
-__version__ = "0.1.0"
+__version__ = "0.1.1"
