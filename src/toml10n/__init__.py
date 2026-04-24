e5d3d46a54375e1bc0481af15f4ac7ff019a8b14
Pon4ick <ikonstantin2010@gmail.com>
2026-04-24 18:13:45 +0300

    0.1.3

--- src/toml10n/__init__.py
+++ src/toml10n/__init__.py
@@ -1,4 +1,4 @@
 from .manager import LocalizationManager, LocaleDirNotFoundError, LanguageNotFoundError, LocaleFileLoadError
 
 __all__ = ["LocalizationManager", "LocaleDirNotFoundError", "LanguageNotFoundError", "LocaleFileLoadError"]
-__version__ = "0.1.2"
+__version__ = "0.1.3"
